d89537dda467b1309a8caf257c7574af48291bf9
scarter31191 <scarter31191@gmaile.com>
2021-01-23 20:01:04 -0500

    created destination index, new, and show action

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -8,11 +8,29 @@ class DestinationsController < ApplicationController
         @destination = Destination.new
     end
 
+    def create
+        @destination = Destination.new(destination_params(:location, :description))
+        @destination.save
+        redirect_to destination_path(@destination)
+    end
+
     def show
-        
+        @destination = Destination.find(params[:id])
     end
 
     def edit
 
+    end
+
+    def destroy
+        @destination = Destination.find(params[:id]).destroy
+        redirect_to destinations_url
+    end
+
+
+    private
+
+    def destination_params(*args)
+        params.require(:destination).permit(*args)
     end
 end
